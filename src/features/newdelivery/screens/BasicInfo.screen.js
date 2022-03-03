db1f72c084d2a625e94658f9bd44dede5c5acaab
jeansn <jeansn@tfg.co.za>
2022-03-03 23:05:23 +0200

    replace formik with react-hook-form

--- src/features/newdelivery/screens/BasicInfo.screen.js
+++ src/features/newdelivery/screens/BasicInfo.screen.js
@@ -1,22 +1,37 @@
 import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Box, Button, VStack, FormControl, Input, Text, Heading, Select, ScrollView, KeyboardAvoidingView, CheckIcon } from 'native-base';
-import { Formik } from 'formik';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 import { Platform } from 'react-native';
+import Icon from 'react-native-ionicons';
 import { DeliveriesContext } from '../../../services/deliveries/Deliveries.context';
 import { NewDeliveryContext } from '../../../services/newDeliveries/NewDelivery.context';
 
-const deliverySchema = yup.object({
-  OrderNumber: yup.string().required(),
-  SiteName: yup.string().required(),
-  AccountNumber: yup.string().required(),
-});
+const schema = yup
+  .object({
+    ordernumber: yup.string().required('Field is required'),
+    sitename: yup.string().required('Field is required'),
+    accnumber: yup.string().required('Field is required'),
+  })
+  .required();
 
 export const BasicInfo = () => {
   const { sites } = useContext(DeliveriesContext);
   const { delivery, UPDATE_FORM } = useContext(NewDeliveryContext);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({
+    defaultValues: { ...delivery },
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => UPDATE_FORM({ ...delivery, ...data });
 
   useEffect(() => {
     console.log(delivery);
@@ -25,112 +40,76 @@ export const BasicInfo = () => {
   return (
     <Box pt={4} flex={1} bg="white">
       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : null} keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0} style={{ flex: 1 }}>
-        <Box flex={1}>
+        <Box flex={1} pt={5}>
           <Heading size="xl" textAlign="center">
             Site Information
           </Heading>
-          <ScrollView
-            flex={1}
-            _contentContainerStyle={{
-              px: '20px',
-              mb: '2',
-              minW: '72',
-            }}
-          >
+          <ScrollView flex={1} _contentContainerStyle={{ px: '20px' }}>
             <Box mt={8} flex={1} justifyContent="space-between">
-              <Formik
-                validationSchema={deliverySchema}
-                initialValues={{
-                  ...delivery,
-                }}
-                onSubmit={(values) => {
-                  console.log(values);
-                  UPDATE_FORM({ ...delivery, ...values });
-                }}
-              >
-                {(formProps) => (
-                  <VStack width="100%" px={5} space={8}>
-                    <FormControl isRequired isInvalid={'OrderNumber' in formProps.errors}>
-                      <FormControl.Label
-                        _text={{
-                          bold: true,
-                        }}
-                        pl={2}
-                      >
-                        Order Number
-                      </FormControl.Label>
-                      <Input
-                        variant="underlined"
-                        placeholder="1234567"
-                        onChangeText={formProps.handleChange('OrderNumber')}
-                        value={formProps.values.OrderNumber}
-                        onBlur={formProps.handleBlur('OrderNumber')}
-                        size="xl"
-                        autoCapitalize="characters"
-                      />
-                      <FormControl.ErrorMessage>{formProps.touched.OrderNumber && formProps.errors.OrderNumber}</FormControl.ErrorMessage>
-                    </FormControl>
-                    <FormControl isRequired isInvalid={'SiteName' in formProps.errors}>
-                      <FormControl.Label
-                        _text={{
-                          bold: true,
-                        }}
-                        pl={2}
-                      >
-                        Site Name
-                      </FormControl.Label>
+              <VStack width="100%" px={5} space={6}>
+                <FormControl isRequired isInvalid={'ordernumber' in errors}>
+                  <FormControl.Label>Order Number</FormControl.Label>
+                  <Controller
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input autoCapitalize="characters" onBlur={onBlur} placeholder="O123456" onChangeText={(val) => onChange(val)} value={value} />
+                    )}
+                    name="ordernumber"
+                    defaultValue={delivery.ordernumber}
+                  />
+                  <FormControl.ErrorMessage>{errors.ordernumber?.message}</FormControl.ErrorMessage>
+                </FormControl>
+
+                <FormControl isRequired isInvalid={'sitename' in errors}>
+                  <FormControl.Label>Select Site:</FormControl.Label>
+                  <Controller
+                    control={control}
+                    render={({ field: { onChange, value } }) => (
                       <Select
-                        variant="underlined"
-                        selectedValue={formProps.values.SiteName}
-                        onBlur={formProps.handleBlur('SiteName')}
-                        minWidth="200"
-                        accessibilityLabel="Choose Site"
-                        placeholder="Choose Site"
-                        _selectedItem={{
-                          bg: 'gray.300',
-                          endIcon: <CheckIcon size="5" />,
+                        placeholder="Select Site"
+                        selectedValue={value}
+                        onValueChange={(itemValue) => {
+                          onChange(itemValue);
+                          setValue('accnumber', sites.filter((site) => site.SiteName === value)[0].AccountNumber);
                         }}
-                        mt={1}
-                        onValueChange={(value) => {
-                          formProps.handleChange('SiteName');
-                          formProps.setFieldValue('AccountNumber', sites.filter((site) => site.SiteName === value)[0].AccountNumber);
-                        }}
-                        size="xl"
+                        selectedItemBg="teal.400"
+                        dropdownOpenIcon={<Icon name="arrow-drop-up" type="MaterialIcons" size={6} />}
+                        dropdownCloseIcon={<Icon name="arrow-drop-down" type="MaterialIcons" size={6} />}
                       >
                         {sites.map((site) => (
                           <Select.Item key={site.Id} label={site.SiteName} value={site.SiteName} />
                         ))}
                       </Select>
-                      <FormControl.ErrorMessage>{formProps.touched.SiteName && formProps.errors.SiteName}</FormControl.ErrorMessage>
-                    </FormControl>
-                    <FormControl isRequired isInvalid={'AccountNumber' in formProps.errors}>
-                      <FormControl.Label
-                        _text={{
-                          bold: true,
-                        }}
-                        pl={2}
-                      >
-                        Account Number
-                      </FormControl.Label>
-                      <Input variant="underlined" value={formProps.values.AccountNumber} size="xl" isDisabled="true" />
-                      <FormControl.ErrorMessage>{formProps.touched.AccountNumber && formProps.errors.AccountNumber}</FormControl.ErrorMessage>
-                    </FormControl>
-                    <Button
-                      isDisabled={!formProps.isValid || Object.keys(formProps.values).length === 0}
-                      size="lg"
-                      onPress={() => {
-                        console.log(formProps);
-                        formProps.handleSubmit();
-                        /*  navigation.navigate('Tank1Screen'); */
-                      }}
-                      mt="5"
-                      colorScheme="primary"
-                    >
-                      Save & Continue
-                    </Button>
-                  </VStack>
-                )}
-              </Formik>
+                    )}
+                    name="sitename"
+                    defaultValue={delivery.sitename}
+                  />
+                  <FormControl.ErrorMessage>{errors.language?.message}</FormControl.ErrorMessage>
+                </FormControl>
+
+                <FormControl isRequired isInvalid={'accnumber' in errors}>
+                  <FormControl.Label>Account Number</FormControl.Label>
+                  <Controller
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <Input autoCapitalize="characters" onBlur={onBlur} placeholder="A123456" onChangeText={(val) => onChange(val)} value={value} />
+                    )}
+                    name="accnumber"
+                    defaultValue={delivery.accnumber}
+                  />
+                  <FormControl.ErrorMessage>{errors.accnumber?.message}</FormControl.ErrorMessage>
+                </FormControl>
+
+                <Button
+                  /*  isDisabled={!formProps.isValid || Object.keys(formProps.values).length === 0} */
+                  size="lg"
+                  onPress={handleSubmit(onSubmit)}
+                  mt="5"
+                  colorScheme="primary"
+                >
+                  Save & Continue
+                </Button>
+              </VStack>
             </Box>
           </ScrollView>
         </Box>
